4bf98b4ca19b74bc3fe40f81bf2d6f5b51eb9174
kayryu <lukayryu@gmail.com>
2019-06-24 17:54:53 +0800

    init

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default class extends React.Component {
+
+
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <title>My page title</title>
+                    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                </div>
+                <div>
+                    <p>输入你的Ladder地址领取测试币</p>
+                    <p>接收地址：</p>
+                    <text></text>
+                    <button>领取</button>
+                </div>
+            </div>
+        )
+    }
+}
