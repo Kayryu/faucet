43e77e3b1f0bef72ffb91eafba3dfa9a4d065402
kayryu <lukayryu@gmail.com>
2019-06-25 07:18:29 -0700

    add docerfile ignore

--- backend.config.js
+++ backend.config.js
@@ -1,5 +1,5 @@
 const url = "ws://122.112.207.62:9944/";
-const key =  "0x311f2d29d68cc7ae6101c3d989f89bb0698c280ddf5ce3a0321f914bda553257";
+const key =  "0x111f2d29d68cc7ae6101c3d989f89bb0698c280ddf5ce3a0321f914bda553257";
 
 module.exports = {
     url,
